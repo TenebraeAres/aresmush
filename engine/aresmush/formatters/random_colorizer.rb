d22538dac1e369b3f0f4c0f573c9e0bc53ed6bf3
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-01-13 17:52:25 -0800

    Update random_colorizer.rb

--- engine/aresmush/formatters/random_colorizer.rb
+++ engine/aresmush/formatters/random_colorizer.rb
@@ -3,7 +3,18 @@ module AresMUSH
     
     # Randomly rotates between colors in a list, based on the seconds value of the current time.
     def self.random_color
-      colors = Global.read_config("skin", "random_colors") || [ '%xc', '%xb', '%xg', '%xr' ]
+    #	colors = Global.read_config("skin", "random_colors") || [ '%xc', '%xb', '%xg', '%xr' ]
+	  Month = DateTime.new.month 
+	  case month
+		when 3 .. 5
+			colors = [ '%x155', '', '', '' ]
+		when 6 .. 8
+			colors = [ '%196', '', '', '' ]
+		when 9 .. 11
+			colors = [ '%x214', '', '', '' ]
+		else
+			colors = [ '%x51', '', '', '' ]
+	  end
       bracket_width = 60 / colors.count
       index = Time.now.sec / bracket_width
       colors[index]
