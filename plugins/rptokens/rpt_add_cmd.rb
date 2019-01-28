47cccb6b88ac3d49918fbad5508a263a842cc3e4
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-01-27 20:42:15 -0800

    Update rpt_add_cmd.rb

--- plugins/rptokens/rpt_add_cmd.rb
+++ plugins/rptokens/rpt_add_cmd.rb
@@ -2,7 +2,7 @@ module AresMUSH
 	module RPTokens
 		class RPTAddCmd
 			include CommandHandler
-			attr_accessor :title, :desc
+			attr_accessor :name, :number, :reason
 			def parse_args	
 				args = cmd.parse_args(ArgParser.arg1_equals_arg2_slash_arg3)
 				self.name = args.arg1
