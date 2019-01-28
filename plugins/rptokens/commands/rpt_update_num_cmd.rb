d8796b50d7e12f9bf25a63d7e075b74cf1735eff
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-01-28 01:15:09 -0800

    RpTokens STILL

--- plugins/rptokens/commands/rpt_update_num_cmd.rb
+++ plugins/rptokens/commands/rpt_update_num_cmd.rb
@@ -1,14 +1,10 @@
 module AresMUSH
 	module RPTokens
-		class  RPTUpdateNun
+		class  RPTUpdateNum
 			
-			attr_accessor :name, :number, :reason
-			
-			self.name = model.name
-			self.number = number
-			self.reason = reason
-			
-			model.update(rpt: self.name.rpt.to_i + self.number)
+			def self.RPTUpdateNum(name, number, reason)
+				name.update(rpt: name.rpt.to_i + self.number)
+			end
 		end
 	end
 end
