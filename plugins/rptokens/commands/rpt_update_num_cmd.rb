861e83fcf6993fa5592e2413fe52d8b02d65ec75
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-01-28 01:33:15 -0800

    RPTokens Work

--- plugins/rptokens/commands/rpt_update_num_cmd.rb
+++ plugins/rptokens/commands/rpt_update_num_cmd.rb
@@ -4,6 +4,7 @@ module AresMUSH
 			
 			def self.rpt_update(name, number, reason)
 				name.update(rpt: name.rpt.to_i + number)
+				client.emit_success "#{number} RP Tokens given to #{name}."
 			end
 		end
 	end
