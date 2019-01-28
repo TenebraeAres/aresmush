3c070699860ce4614df441b5bd41618b0ae04036
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-01-28 01:42:53 -0800

    RPTokens

--- plugins/rptokens/commands/rpt_add_cmd.rb
+++ plugins/rptokens/commands/rpt_add_cmd.rb
@@ -27,6 +27,7 @@ module AresMUSH
 
 			def handle
 				ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
+					client.emit_success "#{self.number} RP Tokens given to #{model.name}."
 					return RPTUpdateNum.rpt_update(model, self.number, self.reason, enactor)
 				end
 			end
