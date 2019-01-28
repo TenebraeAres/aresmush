40dfb14998e245efef29caf96d3c886a88fd1c54
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-01-27 20:49:47 -0800

    Update rpt_add_cmd.rb

--- plugins/rptokens/rpt_add_cmd.rb
+++ plugins/rptokens/rpt_add_cmd.rb
@@ -5,7 +5,7 @@ module AresMUSH
 			attr_accessor :name, :number, :reason
 			def parse_args	
 				args = cmd.parse_args(ArgParser.arg1_equals_arg2_slash_arg3)
-				self.name = args.arg1
+				self.name = Character.find_one_by_name(args.arg1)
 				self.number = integer_arg(args.arg2)
 				self.reason = args.arg3
 			end
@@ -15,7 +15,12 @@ module AresMUSH
 			end
 
 			def handle
-				 client.emit_success "RP Tokens: #{self.name} #{self.number} #{self.reason}" 
+				if self.name.blank?
+					client.emit_fail "Needs a valid player"
+				elsif self.number = 0
+					client.emit_fail "Needs a valid number of RPTokens"
+				else
+					client.emit_success "RP Tokens: #{self.name} #{self.number} #{self.reason}" 
 			end
 		end
 	end
