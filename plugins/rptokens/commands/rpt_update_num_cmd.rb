9cefb29fd5db45aa614c3cc2b4b8ff1d1aa61352
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-01-28 01:16:47 -0800

    Blah

--- plugins/rptokens/commands/rpt_update_num_cmd.rb
+++ plugins/rptokens/commands/rpt_update_num_cmd.rb
@@ -2,7 +2,7 @@ module AresMUSH
 	module RPTokens
 		class  RPTUpdateNum
 			
-			def self.RPTUpdateNum(name, number, reason)
+			def self.rpt_update(name, number, reason)
 				name.update(rpt: name.rpt.to_i + self.number)
 			end
 		end
