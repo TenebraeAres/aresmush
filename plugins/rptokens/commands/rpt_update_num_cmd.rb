6ef7b34c077e34025c37fff86723a3ea2b570a48
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-01-28 01:05:55 -0800

    Update rpt_update_num_cmd.rb

--- plugins/rptokens/commands/rpt_update_num_cmd.rb
+++ plugins/rptokens/commands/rpt_update_num_cmd.rb
@@ -4,9 +4,9 @@ module AresMUSH
 			
 			attr_accessor :name, :number, :reason
 			
-			:name = name
-			:number = number
-			:reason = reason
+			self.name = name
+			self.number = number
+			self.reason = reason
 			
 			self.name.update(rpt: self.name.rpt.to_i + self.number)
 		end
