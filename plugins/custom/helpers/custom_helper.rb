2117601b0942b0aff39b30b15da4964d71c32b21
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-02-06 10:03:51 -0800

    Update custom_helper.rb

--- plugins/custom/helpers/custom_helper.rb
+++ plugins/custom/helpers/custom_helper.rb
@@ -3,7 +3,7 @@ module AresMUSH
 	
 		def self.alt_check(char1, char2)
 			if char1.name != char2.name
-				return nil if enactor.is_admin?
+				return nil if char1.is_admin?
 				return nil if AresCentral.are_chars_linked?(char1, char2)
 				return t('dispatcher.not_allowed')
 			end
