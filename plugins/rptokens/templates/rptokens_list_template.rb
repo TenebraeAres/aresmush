e5b8232d1e851ac78ee5b6b1745d702a559410ee
TenebraeAres <40340822+TenebraeAres@users.noreply.github.com>
2019-02-06 16:45:34 -0800

    NOPE!

--- plugins/rptokens/templates/rptokens_list_template.rb
+++ plugins/rptokens/templates/rptokens_list_template.rb
@@ -7,11 +7,6 @@ module AresMUSH
 			
 			def initialize(char)
 				@char = char
-				if cmd.switch == "all"
-					@loglist = char.rpt_log
-				else
-					@loglist = char.rpt_log.to_a.last(20)
-				end
 				super File.dirname(__FILE__) + "/rptokens_list.erb"
 			end
 		end
